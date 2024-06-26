5006f97ef6d30b42cd1f61298817252c3b37de94
Yusra A. <84106371+CodeX-y@users.noreply.github.com>
2024-06-26 21:42:14 +0000

    completed part 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,25 @@
 Rails.application.routes.draw do
   root "movies#index"
 
-  # Routes for the Movie resource:
+  resources :movies
+
+  # # Routes for the Movie resource:
 
 
-  # CREATE
-  post "/movies" => "movies#create", as: :movies
-  get "/movies/new" => "movies#new", as: :new_movies
+  # # CREATE
+  # post "/movies" => "movies#create", as: :movies
+  # get "/movies/new" => "movies#new", as: :new_movies
 
-  # READ
-  get "/movies" => "movies#index", as: :movie
-  get "/movies/:id" => "movies#show", as: :details
+  # # READ
+  # get "/movies" => "movies#index", as: :movie
+  # get "/movies/:id" => "movies#show", as: :details
   
-  # UPDATE
-  patch "/movies/:id" => "movies#update"
-  get "/movies/:id/edit" => "movies#edit", as: :edit_movie
+  # # UPDATE
+  # patch "/movies/:id" => "movies#update"
+  # get "/movies/:id/edit" => "movies#edit", as: :edit_movie
   
-  # DELETE
-  delete "/movies/:id" => "movies#destroy"
+  # # DELETE
+  # delete "/movies/:id" => "movies#destroy"
 
-  #------------------------------
+  # #------------------------------
 end
